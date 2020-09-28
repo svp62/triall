9661b4aefca668a0f46fb09629b413d503cc7ddd
EC2 Default User <ec2-user@ip-172-31-21-76.ec2.internal>
2020-09-28 01:44:14 +0000

    adding and importing libraries

---
+++ sponacular.py
@@ -0,0 +1,13 @@
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import requests
+import os
+import random
+from os.path import join, dirname
+from dotenv import load_dotenv
+import json
+import flask
+load_dotenv()
