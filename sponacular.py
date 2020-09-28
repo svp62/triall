ed083244210cea78d4a40ed95735bed1d572e948
EC2 Default User <ec2-user@ip-172-31-21-76.ec2.internal>
2020-09-28 01:50:16 +0000

    adding key variables

--- sponacular.py
+++ sponacular.py
@@ -14,6 +14,18 @@ load_dotenv()
 
 app=flask.Flask(__name__)
 
+dotenv_path = join(dirname(__file__), 'spoonacular.env')
+load_dotenv(dotenv_path)
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_token = os.environ['ACCESS_TOKEN']
+access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
+spoonacular_key = os.environ['SPOONACULAR_KEY']
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+auth_api = API(auth, wait_on_rate_limit=True)
 
 
 
